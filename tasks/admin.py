4ef20ef92ed6487119e00d63e7e9418df308f2d0
Avila-Pato <p.avilaf1998@gmail.com>
2025-01-28 00:19:36 -0300

    Add Task model and admin configuration with initial migration

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "description",
+        "created",
+        "datecompleted",
+        "important",
+        "user",
+    ]
+
+
+admin.site.register(Task, TaskAdmin)
